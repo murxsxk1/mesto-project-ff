e15baf1713d888047ad9a45061f8b6c1bea00325
Danila <novash.danila@yandex.by>
2025-03-27 11:15:03 +0300

    Version 1.2 Webpack init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './pages/index.css';
+import { initialCards } from './scripts/cards.js';
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector("#card-template").content;
 
